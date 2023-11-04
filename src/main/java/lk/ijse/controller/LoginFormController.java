3add48d1e94ff842214ad4a4d9be114eaa376a43
theekshana-dilshan <sptheekshanadilshan@gmail.com>
2023-11-04 21:49:32 +0530

    mvc achitecher

--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -1,4 +1,27 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane root;
+    public JFXButton btnLogin;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/view/DashboardForm.fxml"));
+        Scene scene = new Scene(parent);
+
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+    }
 }
